47e3611fc0c41864855217bc9fc02e3b95face2c
raphisto <73787869+raphisto@users.noreply.github.com>
2020-11-03 22:25:03 +0100

    Update apply.sh

--- Patch/apply.sh
+++ Patch/apply.sh
@@ -2,25 +2,25 @@
  cd ../../../..
  
  cd packages/apps/Settings
- patch -p1 -b < ../../../device/LAVA/IRISX8/Patch/android_settings_developer_info.patch
+ patch -p1 -b < ../../../device/myphone/luna/Patch/android_settings_developer_info.patch
  git clean -f -d
  
  cd ../../../system/vold
- patch -p1 < ../../device/LAVA/IRISX8/Patch/android_system_vold.patch
+ patch -p1 < ../../device/myphone/luna/Patch/android_system_vold.patch
  
  cd ../../external/icu
- patch -p1 < ../../device/LAVA/IRISX8/Patch/android_external_icu.patch
+ patch -p1 < ../../device/myphone/luna/Patch/android_external_icu.patch
 
  cd ../../frameworks/av
- patch -p1 < ../../device/LAVA/IRISX8/Patch/android_frameworks_av.patch
+ patch -p1 < ../../device/myphone/luna/Patch/android_frameworks_av.patch
  
  cd ..
  cd base
- patch -p1 < ../../device/LAVA/IRISX8/Patch/android_frameworks_base.patch
+ patch -p1 < ../../device/myphone/luna/Patch/android_frameworks_base.patch
  
  cd ../..
  cd packages/apps/FMRadio
- patch -p1 < ../../../device/LAVA/IRISX8/Patch/android_packages_apps_FMRadio.patch
+ patch -p1 < ../../../device/myphone/luna/Patch/android_packages_apps_FMRadio.patch
  
  cd ../../../
  
