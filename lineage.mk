7d10eb0ad6baf85d9f31c9423fd925d433f5f539
raphisto <73787869+raphisto@users.noreply.github.com>
2020-11-14 22:21:16 +0100

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -18,7 +18,7 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Luna
+PRODUCT_DEVICE := myPhone
 PRODUCT_NAME := lineage_myPhone
 PRODUCT_BRAND := myPhone
 PRODUCT_MODEL := Luna
