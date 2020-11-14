4e809dc79a8d477c802904a221d8ac954de6b5c2
raphisto <73787869+raphisto@users.noreply.github.com>
2020-11-14 18:30:31 +0100

    Update apply.sh

--- Test-1/apply.sh
+++ Test-1/apply.sh
@@ -2,59 +2,59 @@
 
  cd ../../../..
  cd system/core
- #patch -p1 < ../../device/myphone/luna/Test-1/android_system_core_MTK.patch
+ #patch -p1 < ../../device/myPhone/Luna/Test-1/android_system_core_MTK.patch
  #echo android_system_core Done
  
  cd ..
  cd bt
- patch -p1 < ../../device/myphone/luna/Test-1/android_system_bt.patch
+ patch -p1 < ../../device/myPhone/Luna/Test-1/android_system_bt.patch
  echo android_system_bt Done
  
  cd ..
  cd netd
- patch -p1 < ../../device/myphone/luna/Test-1/android_system_netd.patch
+ patch -p1 < ../../device/myPhone/Luna/Test-1/android_system_netd.patch
  echo android_system_netd Done
  
  cd ..
  cd sepolicy
- patch -p1 < ../../device/myphone/luna/Test-1/android_system_sepolicy_MTK.patch
+ patch -p1 < ../../device/myPhone/Luna/Test-1/android_system_sepolicy_MTK.patch
  echo android_system_sepolicy - Teja Done
  
  cd ../..
  cd frameworks/av
- #patch -p1 < ../../device/myphone/luna/Test-1/android_frameworks_av.patch
+ #patch -p1 < ../../device/myPhone/Luna/Test-1/android_frameworks_av.patch
  #echo android_frameworks_av Done
  
  cd ..
  cd base
- patch -p1 < ../../device/myphone/luna/Test-1/android_frameworks_base.patch
+ patch -p1 < ../../device/myPhone/Luna/Test-1/android_frameworks_base.patch
  echo android_frameworks_base Done
  
  cd ..
  cd native
- #patch -p1 < ../../device/myphone/luna/Test-1/android_frameworks_native_MTK.patch
+ #patch -p1 < ../../device/myPhone/Luna/Test-1/android_frameworks_native_MTK.patch
  #echo android_frameworks_native Done
  
  cd ..
  cd opt/telephony
- patch -p1 < ../../../device/myphone/luna/Test-1/android_frameworks_opt_telephony.patch
+ patch -p1 < ../../../device/myPhone/Luna/Test-1/android_frameworks_opt_telephony.patch
  echo android_frameworks_opt_telephony Done
  
  cd ../../..
  cd external/icu
- patch -p1 < ../../device/myphone/luna/Test-1/android_external_icu.patch
+ patch -p1 < ../../device/myPhone/Luna/Test-1/android_external_icu.patch
  echo android_external_icu Done
  
  cd ..
  cd wpa_supplicant_8
- patch -p1 < ../../device/myphone/luna/Test-1/android_external_wpa_supplicant_8.patch
+ patch -p1 < ../../device/myPhone/Luna/Test-1/android_external_wpa_supplicant_8.patch
  echo android_external_wpa_supplicant_8 Done
  
  cd ../..
  cd packages/services/Telephony
- patch -p1 -p1 < ../../../device/myphone/luna/Test-1/android_packages_services_Telephony.patch
+ patch -p1 -p1 < ../../../device/myPhone/Luna/Test-1/android_packages_services_Telephony.patch
  echo android_packages_services_Telephony Done
  
  cd ../../..
  
- echo Chaliye Suru Karte Hai...
+ echo apply.sh ok ...
