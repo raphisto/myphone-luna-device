89df0f3fe334fb8086ba2e82708e6aff34d98e36
raphisto <73787869+raphisto@users.noreply.github.com>
2020-11-14 19:29:37 +0100

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -5,7 +5,7 @@ DEVICE_PATH := device/myPhone/Luna
 VENDOR_PATH := vendor/myPhone/Luna
 
 # Release name
-PRODUCT_RELEASE_NAME := lineage_luna
+PRODUCT_RELEASE_NAME := Luna
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -18,8 +18,8 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := myPhone Luna
-PRODUCT_NAME := lineage_luna
+PRODUCT_DEVICE := Luna
+PRODUCT_NAME := lineage_Luna
 PRODUCT_BRAND := myPhone
 PRODUCT_MODEL := Luna
 PRODUCT_MANUFACTURER := myPhone
