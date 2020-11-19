0c1b1fad9707be845ac107ab28ceded83e60d086
raphisto <73787869+raphisto@users.noreply.github.com>
2020-11-19 07:01:37 +0100

    Update rr_Luna.mk

--- rr_Luna.mk
+++ rr_Luna.mk
@@ -144,3 +144,29 @@ PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
 
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+
+# Device path
+DEVICE_PATH := device/myPhone/Luna
+
+# Vendor path
+VENDOR_PATH := vendor/myPhone/Luna
+
+# Release name
+PRODUCT_RELEASE_NAME := Luna
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Luna
+PRODUCT_NAME := rr_Luna
+PRODUCT_BRAND := myPhone
+PRODUCT_MODEL := Luna
+PRODUCT_MANUFACTURER := myPhone
